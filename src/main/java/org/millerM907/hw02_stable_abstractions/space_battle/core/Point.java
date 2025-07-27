ed4c1a7e8059654415698a09a5c47589b7f5d28b
millerM907 <prilivy@mail.ru>
2025-07-27 13:05:17 +0300

    Implement uniform linear motion without deformation
    * Defined an interface resilient to new types of movable objects
    * Encapsulated motion logic in a separate class
    * Applied the Adapter design pattern for object integration
    * Added unit tests to verify behavior

---
+++ src/main/java/org/millerM907/hw02_stable_abstractions/space_battle/core/Point.java
@@ -0,0 +1,30 @@
+package org.millerM907.hw02_stable_abstractions.space_battle.core;
+
+import java.util.Objects;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point plus(Vector vector) {
+        return new Point(this.x + vector.x(), this.y + vector.y());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point other = (Point) obj;
+        return x == other.x && y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
